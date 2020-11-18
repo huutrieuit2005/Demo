e0aa825eba1930f2d7cc2b765286a1d934622e04
huutrieuit2005 <48130582+huutrieuit2005@users.noreply.github.com>
2020-11-18 19:34:10 +0700

    add file

---
+++ src/component/abc.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { delList, editList } from "../stores/employees/actions";
+import { useDispatch } from 'react-redux';
+
+interface Props {
+  index: number;
+}
+const ButtonDelEdit: React.FC<Props> = (Props) => {
+  const dispatch = useDispatch();
+  return(
+    <div className="cell">
+      <button onClick={() => dispatch(delList(Props.index))}>Delete</button>
+      <button onClick={() => dispatch(editList(Props.index))}>Edit</button>
+    </div>
+  );
+}
+
+
+export default ButtonDelEdit;
